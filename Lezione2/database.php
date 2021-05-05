438cf108a94c8b7255f8406f1aa50735d7630517
maboglia <mauro.bogliaccino@gmail.com>
2021-05-05 17:48:47 +0200

    agg

--- Lezione2/database.php
+++ Lezione2/database.php
@@ -8,9 +8,25 @@ $films[] = ['id'=>3, "titolo"=>"THE YOUNG POPE", "genere"=>"commedia"];
 $films[] = ['id'=>4, "titolo"=>"SULLY", "genere"=>"drammatico"];
 
 
-var_dump($films);
+//var_dump($films);
+header("Content-Type: application/json");
 
+if(isset($_GET['id'])){
+    $bool=false;
+    foreach ($films as $key => $value) {
+        if ($films[$key]['id']==$_GET['id']){
+            echo json_encode($films[$key]);
+            $bool = true;
+            break;    
+        }
+    
+        }
+    if (!$bool) 
+        echo '{"errore":"non disponibile"}';
 
+} else {
+    echo json_encode($films);
+}
 
 
 
